be3b9c3c0656606e84b2181d8adc095003b1b795
Vladislav Stoitsov <vlados.01@gmail.com>
2021-12-08 17:54:01 +0200

    test

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -38,8 +38,13 @@ class RouteServiceProvider extends ServiceProvider
         $this->configureRateLimiting();
 
         $this->routes(function () {
+            Route::prefix('api')
+                ->middleware('api')
+                ->namespace($this->namespace)
+                ->group(base_path('routes/api.php'));
 
-            Route::namespace($this->namespace)
+            Route::middleware('web')
+                ->namespace($this->namespace)
                 ->group(base_path('routes/web.php'));
         });
     }
