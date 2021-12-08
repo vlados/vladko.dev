92c614a22544b6e408a777e405bf559c893b7663
Vladislav Stoitsov <vlados.01@gmail.com>
2021-12-08 17:50:18 +0200

    test

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 //Route::view('/', 'welcome')->name('home');
-Route::get('/', [\App\Http\Controllers\HomeController::class,'index'])->name('home');
+Route::get('/', [\App\Http\Controllers\HomeController::class,'index'])->name('home')->withoutMiddleware("web")->withoutBlocking();
 
