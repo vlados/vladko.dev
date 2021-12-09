75cf8ebd64bbdf7fa2802c6b2d4daae6a12551e9
Vladislav Stoitsov <vlados.01@gmail.com>
2021-12-09 18:37:33 +0200

    added missing defer of images in skills section

--- resources/views/components/skill.blade.php
+++ resources/views/components/skill.blade.php
@@ -1,6 +1,6 @@
 @props(["image","text"])
 <div
     class="flex flex-col space-y-3 items-center  w-24 print:w-20 self-center group transition duration-200 hover:bg-white hover:shadow-xl rounded-lg p-4">
-    <img src="{{ asset("images/skills/".$image) }}" class="h-12 print:h-10" alt="{{$text}}" {{ $attributes }}/>
+    <img src="{{ asset("images/skills/".$image) }}" class="h-12 print:h-10" alt="{{$text}}" {{ $attributes }} loading="lazy"/>
     <span class="leading-3 text-center text-xs text-gray-600 font-semibold">{{ $text }}</span>
 </div>
