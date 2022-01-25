51bc9b5f3addaecb4eee2f6eab8f40eeb8f55ac5
Vladislav Stoitsov <vlados.01@gmail.com>
2022-01-25 22:43:07 +0200

    Remove bootstrap/cache from the repo, It should be generated per enviorment

--- bootstrap/cache/livewire-components.php
+++
@@ -1,5 +0,0 @@
-<?php return array (
-  'contact' => 'App\\Http\\Livewire\\Contact',
-  'project-details' => 'App\\Http\\Livewire\\ProjectDetails',
-  'reactions' => 'App\\Http\\Livewire\\Reactions',
-);
