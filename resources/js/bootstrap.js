a5c151b2dddfea52e219fda7e2e8960e95733277
Shift <shift@laravelshift.com>
2023-02-13 12:25:14 +0000

    Rename ENV variables

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -14,7 +14,7 @@ Alpine.start();
 
 // window.Echo = new Echo({
 //     broadcaster: 'pusher',
-//     key: process.env.MIX_PUSHER_APP_KEY,
-//     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
+//     key: import.meta.env.VITE_PUSHER_APP_KEY,
+//     cluster: import.meta.env.VITE_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
